c33c84b18dfb53ba1de08602486015992ef9b32d
k.petrova <petrova@justcontrol.it>
2021-09-10 23:18:56 +0600

    added files

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+/* global $ */
+$(document).ready(function () {
+    $('.js-nav-btn').click(function () {
+        $(this).toggleClass('_active');
+        $('.js-nav-body').toggleClass('_active');
+        $('body').toggleClass('locked');
+    });
+});
