c16f0338587b9317413970dc4e71d0f3c945aebb
k.petrova <petrova@justcontrol.it>
2021-09-11 02:57:24 +0600

    added files

---
+++ app/js/animated-inview.js
@@ -0,0 +1,58 @@
+(function($) {
+	var watchedElements = [];
+	var doc = document.documentElement;
+	var currentYScroll = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+	$('.in-view-watcher').each(function(i, v) {
+		watchedElements.push($(v));
+	});
+
+	// Add the jQuery function "inViewWatcher()"
+	$.fn.extend({
+		inViewWatcher: function() {
+			watchedElements.push(this);
+			return this;
+		},
+	});
+
+	function checkWatchedElements() {
+		// Get the current offset scrolled from the top
+		currentYScroll = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+		// Get the window's height
+		var clientHeight = $(window).height();
+
+		// Loop through each of the elements gathered by the selector above this scroll event
+		var newWatchedElementsList = [];
+		$.each(watchedElements, function(_, obj) {
+			if (typeof obj === 'undefined') {
+				return;
+			}
+
+			// Get the distance from the top of the document to the top of this element
+			var topOfElement = obj.offset().top;
+			// Get the distance from the top of the document to the bottom of this element
+			var bottomOfElement = obj.offset().top + obj.height();
+
+			if (
+				// Check if the top of the element is in the viewport
+				(topOfElement > currentYScroll && topOfElement < currentYScroll + clientHeight) ||
+				// Or check if the bottom element is in view
+				(bottomOfElement > currentYScroll && bottomOfElement < currentYScroll + clientHeight)
+			) {
+				obj.addClass('is-in-view');
+				obj.trigger('in-view');
+			} else {
+				newWatchedElementsList.push(obj);
+			}
+		});
+
+		// Must be changed here, if changed in the loop (spliced) then some elements will be skipped
+		watchedElements = newWatchedElementsList;
+	}
+
+	// Register to the scroll event
+	$(window).on('scroll', checkWatchedElements);
+	$(window).on('load', checkWatchedElements);
+	// Run the function once to see if the plugin needs to add the class to any currently viewable elements before a scroll event
+	checkWatchedElements();
+})(jQuery);
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,51 @@
 /* global $ */
+
 $(document).ready(function () {
+    let watchedElements = [];
+    let doc = document.documentElement;
+    let currentYScroll = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    $('.in-view-watcher').each(function(i, v) {
+        watchedElements.push($(v));
+    });
+    $.fn.extend({
+        inViewWatcher: function() {
+            watchedElements.push(this);
+            return this;
+        },
+    });
+    function checkWatchedElements() {
+        currentYScroll = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+        let clientHeight = $(window).height();
+        let newWatchedElementsList = [];
+        $.each(watchedElements, function(_, obj) {
+            if (typeof obj === 'undefined') {
+                return;
+            }
+            let topOfElement = obj.offset().top;
+            let bottomOfElement = obj.offset().top + obj.height();
+
+            if (
+                (topOfElement > currentYScroll && topOfElement < currentYScroll + clientHeight) ||
+                (bottomOfElement > currentYScroll && bottomOfElement < currentYScroll + clientHeight)
+            ) {
+                obj.addClass('is-in-view');
+                obj.trigger('in-view');
+            } else {
+                newWatchedElementsList.push(obj);
+            }
+        });
+        watchedElements = newWatchedElementsList;
+    }
+    $(window).on('scroll', checkWatchedElements);
+    $(window).on('load', checkWatchedElements);
+    checkWatchedElements();
+
+    $(".in-view-watcher").each((i, item) => {
+        console.log(i);
+        $(item).css("animation-delay", 0.3 * (i + 1) + "s");
+    });
+
+
     $('.js-nav-btn').click(function () {
         $(this).toggleClass('active');
         $('.js-nav-body').toggleClass('active');
